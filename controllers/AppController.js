3f2361451d5b100d202da4d7c35fa6551f3cc2df
AhmedEhab2022 <am8122004@gmail.com>
2024-06-29 23:25:03 +0300

    add await to methods

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,16 +4,16 @@ const dbClient = require('../utils/db');
 class AppController {
   static async getStatus(request, response) {
     const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+      redis: await redisClient.isAlive(),
+      db: await dbClient.isAlive(),
     };
     response.status(200).send(status);
   }
 
   static async getStats(request, response) {
     const stats = {
-      users: dbClient.nbUsers(),
-      files: dbClient.nbFiles(),
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
     };
     response.status(200).send(stats);
   }
