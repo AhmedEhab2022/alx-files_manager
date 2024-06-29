5716ab6107aa0e4950a767d89cd366ed86f9eb9d
AhmedEhab2022 <am8122004@gmail.com>
2024-06-30 01:17:38 +0300

    change key name of the password in db

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,16 +3,20 @@ const dbClient = require('../utils/db');
 
 class UsersController {
   static async postNew(request, response) {
-    const { email, password } = request.body;
+    const { email } = request.body;
+    const password = sha1(request.body.password);
     if (!email) return response.status(400).send({ error: 'Missing email' });
     if (!password) return response.status(400).send({ error: 'Missing password' });
-    const userExist = await dbClient.db.collection('users').findOne({ email });
-    if (userExist) {
-      return response.status(400).send({ error: 'Already exist' });
+    try {
+      const userExist = await dbClient.db.collection('users').findOne({ email });
+      if (userExist) {
+        return response.status(400).send({ error: 'Already exist' });
+      }
+      const user = await dbClient.db.collection('users').insertOne({ email, password });
+      return response.status(201).send({ id: user.insertedId, email });
+    } catch (error) {
+      return response.status(500).send({ error: 'Internal Server Error' });
     }
-    const hashedPassword = sha1(password);
-    const user = await dbClient.db.collection('users').insertOne({ email, hashedPassword });
-    return response.status(201).send({ id: user.insertedId, email });
   }
 }
 
