7dda0a0e0d0d6e6ddc760bf5a9757f011baa34af
AhmedEhab2022 <am8122004@gmail.com>
2024-06-30 01:24:35 +0300

    change password to regular variable

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,7 +4,7 @@ const dbClient = require('../utils/db');
 class UsersController {
   static async postNew(request, response) {
     const { email } = request.body;
-    const password = sha1(request.body.password);
+    let { password } = request.body;
     if (!email) return response.status(400).send({ error: 'Missing email' });
     if (!password) return response.status(400).send({ error: 'Missing password' });
     try {
@@ -12,6 +12,7 @@ class UsersController {
       if (userExist) {
         return response.status(400).send({ error: 'Already exist' });
       }
+      password = sha1(password);
       const user = await dbClient.db.collection('users').insertOne({ email, password });
       return response.status(201).send({ id: user.insertedId, email });
     } catch (error) {
