f6914d589cffbf8a3bb1487feb83824c1d8ab35a
AhmedEhab2022 <am8122004@gmail.com>
2024-06-30 14:01:32 +0300

    edit postUpload method

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const uuid = require('uuid');
+const path = require('path');
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
@@ -22,6 +23,7 @@ class FilesController {
     if (!name) return res.status(400).send({ error: 'Missing name' });
     if (!type) return res.status(400).send({ error: 'Missing type' });
     if (!data && type !== 'folder') return res.status(400).send({ error: 'Missing data' });
+    if (type !== 'folder' && type !== 'file' && type !== 'image') return res.status(400).send({ error: 'Invalid type' });
     if (parentId) {
       const parent = await dbClient.findFileById(parentId);
       if (!parent) return res.status(400).send({ error: 'Parent not found' });
@@ -37,12 +39,14 @@ class FilesController {
       data: data || null,
     };
     if (type !== 'folder') {
-      const path = process.env.FOLDER_PATH || '/tmp/files_manager';
+      const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
       if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
       // Generate a unique filename
-      const filePath = `${path}/${uuid.v4()}`;
+      const filePath = path.join(folderPath, uuid.v4());
+      // Decode the base64 data
+      const fileData = Buffer.from(data, 'base64');
       // Write the file to the filesystem
-      fs.writeFileSync(filePath, data, 'utf-8');
+      fs.writeFileSync(filePath, fileData);
       file.localPath = filePath;
     }
     const newFile = await dbClient.createFile(file);
